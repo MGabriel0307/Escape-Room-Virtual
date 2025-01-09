c136bc50068f06af553467b38e8769f65e31b6d9
MGabriel0307 <popescu.gabriel.t6d@student.ucv.ro>
2025-01-09 21:43:31 +0200

    Update Usa.cpp

--- Usa.cpp
+++ Usa.cpp
@@ -1,20 +1,20 @@
-﻿#include "Usa.h"
+#include "Usa.h"
 #include <iostream>
 using namespace std;
 
-Usa::Usa(bool incuiata, const string& cheiaNecesara)
-    : incuiata(incuiata), cheiaNecesara(cheiaNecesara) {}
+Usa::Usa(bool incuiata, const string& cheieNecesar)
+    : incuiata(incuiata), cheieNecesar(cheieNecesar) {}
 
 bool Usa::deschideUsa(Inventar& inventar) {
     if (!incuiata) {
-        cout << "Ușa este deja descuiată.\n";
+        cout << "Usa este deja deschisa!\n";
         return true;
     }
-    if (inventar.areObiect(cheiaNecesara)) {
-        cout << "Ai folosit cheia: " << cheiaNecesara << ". Ușa s-a descuiat!\n";
+    if (inventar.contineObiect(cheieNecesar)) {
+        cout << "Usa s-a deschis cu succes!\n";
         incuiata = false;
         return true;
     }
-    cout << "Ușa este încuiată. Ai nevoie de cheia: " << cheiaNecesara << ".\n";
+    cout << "Usa este incuiata. Ai nevoie de cheia: " << cheieNecesar << endl;
     return false;
 }
