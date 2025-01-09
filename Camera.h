753d03cbedc7bbf7b8ab7bb15f237e4bc41f1275
MGabriel0307 <popescu.gabriel.t6d@student.ucv.ro>
2025-01-09 21:33:33 +0200

    Update Camera.h

--- Camera.h
+++ Camera.h
@@ -5,19 +5,21 @@
 #include "Usa.h"
 #include "Obiect.h"
 #include <string>
+#include <vector>
 using namespace std;
 
 class Camera {
 private:
     string descriere;
-    Intrebare intrebare;
+    vector<Intrebare> intrebari;
     Usa usa;
     Obiect obiect;
 
 public:
-    Camera(const string& descriere, const Intrebare& intrebare, const Usa& usa, const Obiect& obiect);
+    Camera(const string& descriere, const vector<Intrebare>& intrebari, const Usa& usa, const Obiect& obiect);
 
     string getDescriere() const;
+    void afiseazaIntrebari() const;
     bool rezolvaPuzzle(const string& raspuns, Inventar& inventar);
     void colecteazaObiect(Inventar& inventar);
     bool deschideUsa(Inventar& inventar);
