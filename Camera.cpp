d1f9182ad51df8d0824373567ffda46c95ae11a6
MGabriel0307 <popescu.gabriel.t6d@student.ucv.ro>
2025-01-09 21:30:36 +0200

    Update Camera.cpp

--- Camera.cpp
+++ Camera.cpp
@@ -1,27 +1,35 @@
-﻿#include "Camera.h"
+#include "Camera.h"
 #include <iostream>
 using namespace std;
 
-Camera::Camera(const string& descriere, const Intrebare& intrebare, const Usa& usa, const Obiect& obiect)
-    : descriere(descriere), intrebare(intrebare), usa(usa), obiect(obiect) {}
+Camera::Camera(const string& descriere, const vector<Intrebare>& intrebari, const Usa& usa, const Obiect& obiect)
+    : descriere(descriere), intrebari(intrebari), usa(usa), obiect(obiect) {}
 
 string Camera::getDescriere() const {
     return descriere;
 }
 
-bool Camera::rezolvaPuzzle(const string& raspuns, Inventar& inventar) {
-    if (intrebare.verificaRaspuns(raspuns)) {
-        cout << "Răspuns corect!\n";
-        colecteazaObiect(inventar);
-        return true;
+void Camera::afiseazaIntrebari() const {
+    cout << "Intrebari disponibile:" << endl;
+    for (size_t i = 0; i < intrebari.size(); ++i) {
+        cout << i + 1 << ". " << intrebari[i].getIntrebare() << endl;
     }
-    cout << "Răspuns greșit!\n";
+}
+
+bool Camera::rezolvaPuzzle(const string& raspuns, Inventar& inventar) {
+    for (const auto& intrebare : intrebari) {
+        if (intrebare.verificaRaspuns(raspuns)) {
+            cout << "Raspuns corect!" << endl;
+            colecteazaObiect(inventar);
+            return true;
+        }
+    }
     return false;
 }
 
 void Camera::colecteazaObiect(Inventar& inventar) {
     inventar.adaugaObiect(obiect);
-    cout << "Ai colectat obiectul: " << obiect.getNume() << "!\n";
+    cout << "Ai colectat obiectul: " << obiect.getNume() << "!" << endl;
 }
 
 bool Camera::deschideUsa(Inventar& inventar) {
